9ca4cc14e823ab0e0a3b0d4eef111816b25033e0
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-29 07:19:42 +0700

    Belajar Object
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/app/Object.kt
@@ -0,0 +1,14 @@
+package app
+
+import data.Person
+
+fun main() {
+
+    val daud = Person()
+    val siraj = Person()
+    val bintang = Person()
+
+    println(daud)
+    println(siraj)
+    println(bintang)
+}
