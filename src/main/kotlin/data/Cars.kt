566387747585e3e54310ac3c71b21ad33ef573cb
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-29 09:51:05 +0700

    Belajar Initializer Block
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

--- src/main/kotlin/data/Cars.kt
+++ src/main/kotlin/data/Cars.kt
@@ -4,5 +4,8 @@ class Cars(paramBrand:String, paramName:String, paramColor:String="balck") {
     var brand: String = paramBrand
     var name: String = paramName
     var color: String = paramColor
+    init {
+        println("Car $brand $name $color")
+    }
 
 }
