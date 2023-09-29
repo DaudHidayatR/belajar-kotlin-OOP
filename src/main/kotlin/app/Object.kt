f45828c54ed7222430ec8844b33adb3f63f08fa2
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-29 07:27:19 +0700

    Belajar Properties
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

--- src/main/kotlin/app/Object.kt
+++ src/main/kotlin/app/Object.kt
@@ -5,10 +5,19 @@ import data.Person
 fun main() {
 
     val daud = Person()
+    daud.firstName = "Daud"
+    daud.middleName = "Hidayat"
+    daud.lastName = "Ramadhan"
     val siraj = Person()
+    siraj.firstName = "Muhammad"
+    siraj.middleName = "Rizki"
+    siraj.lastName = "Siraj"
     val bintang = Person()
+    bintang.firstName = "Bintang"
+    bintang.middleName = null
+    bintang.lastName = "Rahmatullah"
 
-    println(daud)
-    println(siraj)
-    println(bintang)
+    println(daud.firstName)
+    println(siraj.lastName)
+    println(bintang.firstName)
 }
