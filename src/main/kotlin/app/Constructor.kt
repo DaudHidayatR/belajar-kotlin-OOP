cd514802149fd10e4136edde14256fad28450327
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-29 10:01:57 +0700

    Belajar Secondary Constuctor
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

--- src/main/kotlin/app/Constructor.kt
+++ src/main/kotlin/app/Constructor.kt
@@ -4,6 +4,7 @@ fun main() {
     val toyota = Cars("Toyota", "Avanza", "Black")
     val honda = Cars("Honda", "Jazz", "White")
     val suzuki = Cars("Suzuki", "Ertiga")
+    val ferrari = Cars("ferrari")
 
     println(toyota.brand)
     println(honda.brand)
--- src/main/kotlin/data/Cars.kt
+++ src/main/kotlin/data/Cars.kt
@@ -1,11 +1,22 @@
 package data
 
-class Cars(paramBrand:String, paramName:String, paramColor:String="balck") {
+class Cars(paramBrand:String, paramName:String, paramColor:String="black") {
+
+    constructor(paramBrand:String, paramName:String): this(paramBrand, paramName, "black"){
+        println("secondary constructor")
+    }
+
+    constructor(paramBrand:String): this(paramBrand, ""){
+        println("secondary constructor 2")
+    }
+
     var brand: String = paramBrand
     var name: String = paramName
     var color: String = paramColor
+
     init {
         println("Car $brand $name $color")
     }
+
 
 }
