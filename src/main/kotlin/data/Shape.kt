7d9e6d481b8947921f9c3b194447877d6db86943
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-10-01 09:30:42 +0700

    Belajar Super Keyword
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

--- src/main/kotlin/data/Shape.kt
+++ src/main/kotlin/data/Shape.kt
@@ -2,7 +2,15 @@ package data
 
 open class Shape {
     open val corner : Int = -1
+    open fun printName() {
+        println("This is shape")
+    }
 }
 class Rectangle : Shape() {
   final override val corner : Int = 4
+    val parentCorner : Int = super.corner
+   override fun printName() {
+        println("This is rectangle")
+        super.printName()
+    }
 }
